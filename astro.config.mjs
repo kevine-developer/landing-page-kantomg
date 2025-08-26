b8b4ce1e278b58d6e6e4bb82cc0aa974ec2ce956
Kevine_dev <kevinarson_1996@yahoo.fr>
2025-08-26 16:40:43 +0200

    Intégration du sitemap et ajout des assets du site
    - Configuration d’Astro pour générer automatiquement un sitemap, améliorant le SEO.
    - Ajout d’un favicon et d’une nouvelle icône pour renforcer l’identité visuelle.
    - Mise à jour de `.gitignore` afin d’exclure les fichiers propres à l’IDE, les fichiers de lock de dépendances et les artefacts de build, gardant le dépôt propre.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()] ,
   },
+site: 'https://www.votresite.com',
+  integrations: [sitemap()],
 });
